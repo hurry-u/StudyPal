360a3b83069ea738d96dacd60047cd43264e760c
Timeless4k <guru12.it@gmail.com>
2023-05-11 00:10:05 +1000

    Menu Setup/Layout

--- StudyPal/LoginView.swift
+++ StudyPal/LoginView.swift
@@ -14,8 +14,8 @@ struct LoginView: View {
     @State private var rememberMe = false
     
     var body: some View {
-        NavigationView{
-            VStack(spacing: 20){
+        NavigationView {
+            VStack(spacing: 20) {
                 VStack {
                     Image("Logo")
                         .resizable()
@@ -23,31 +23,79 @@ struct LoginView: View {
                         .frame(height: 100)
                         .padding(.top, 100)
                 }
+                
                 Spacer()
                 
-                VStack(){
+                VStack(alignment: .leading, spacing: 10) {
                     Text("Email")
                         .foregroundColor(.gray)
-                    TextField("",text:$email)
+                    TextField("john.smith@gmail.com", text: $email)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .autocapitalization(.none)
                         .multilineTextAlignment(.center)
                 }
                 
-                VStack(){
+                VStack(alignment: .leading, spacing: 10) {
                     Text("Password")
                         .foregroundColor(.gray)
-                    TextField("",text:$email)
+                    SecureField("password", text: $password)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .autocapitalization(.none)
                         .multilineTextAlignment(.center)
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    // Check if the email and password are valid
+                    if email == "john.smith@gmail.com" && password == "password" {
+                        print("Logged in successfully!")
+                        isLoggedIn = true
+                    }
+                }) {
+                    NavigationLink(destination: GreetingView(), isActive: $isLoggedIn) {
+                        Text("Log in")
+                            .frame(maxWidth: .infinity, minHeight: 44)
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                    }
+                }
+                
+                
+                HStack(spacing: 20) {
+                    Button(action: {
+                        // Sign up with Google
+                    }) {
+                        Image("google")
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                            .padding(8)
+                            .background(Color("GoogleColor"))
+                            .clipShape(Circle())
+                    }
+                    
+                    Button(action: {
+                        // Sign up with Facebook
+                    }) {
+                        Image("facebook")
+                            .resizable()
+                            .frame(width: 24, height: 24)
+                            .padding(8)
+                            .background(Color("FacebookColor"))
+                            .clipShape(Circle())
+                    }
                 }
                 Spacer()
             }
-            
+            .padding(.bottom, 20)
+            .padding(.horizontal, 20)
         }
     }
 }
+    
+
+
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
