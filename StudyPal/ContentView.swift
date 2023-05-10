0606edf08d204784fbbe443e4550b7e04a606c74
hurry-u <sana.chandna27@gmail.com>
2023-05-10 15:21:01 +1000

    Initial Commit

---
+++ StudyPal/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StudyPal
+//
+//  Created by Sana Chandna on 10/5/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudyPal/StudyPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyPalApp.swift
+//  StudyPal
+//
+//  Created by Sana Chandna on 10/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct StudyPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
