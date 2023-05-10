e37aa9c7414d8936de243e51ecbeace55b20dca3
hurry-u <sana.chandna27@gmail.com>
2023-05-10 16:02:16 +1000

    Files

--- StudyPal/StudyPalApp.swift
+++ StudyPal/StudyPalApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct StudyPalApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            let loginView = LoginView()
+            loginView
         }
     }
 }
