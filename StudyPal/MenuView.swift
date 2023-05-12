a0ed06d95d698a689dc89f222683179cd8a7663c
Timeless4k <guru12.it@gmail.com>
2023-05-12 10:48:14 +1000

    Error & Layout of Menu

--- StudyPal/MenuView.swift
+++ StudyPal/MenuView.swift
@@ -2,20 +2,20 @@
 //  MenuView.swift
 //  StudyPal
 //
-//  Created by Gurudeep Dhinjan on 11/5/2023.
+//  Created by Sana Chandna & Gurudeep Dhinjan on 10/5/2023.
 //
 
 import SwiftUI
 
 struct MenuView: View {
-    @State private var currentQuote = Quote.getRandomQuote()
+    //    @State private var currentQuote = Quote.getRandomQuote()
     @State private var isDeepFocusModeOn = false
     @State private var minutes = 30
     @State private var task = ""
     @State private var timeRemaining = 0.0
     @State private var timer: Timer?
     @State private var progressValue: CGFloat = 1.0
-
+    
     let progressBarWidth: CGFloat = 300.0
     let progressBarHeight: CGFloat = 20.0
     let progressBarCornerRadius: CGFloat = 10.0
@@ -29,11 +29,10 @@ struct MenuView: View {
                 self.isDeepFocusModeOn = false
                 self.timer?.invalidate()
                 self.timer = nil
-//                let newTask = Task(taskName: self.task, dueDate: Date(), priority: .low, tags: [])
-//                toDoList.addTask(newTask)
             }
         }
     }
+    
     func stopTimer() {
         timer?.invalidate()
         isDeepFocusModeOn = false
@@ -42,6 +41,37 @@ struct MenuView: View {
     }
     
     func clearTimer() {
-            timeRemaining = 0
-            progressValue = 1.0
+        timeRemaining = 0
+        progressValue = 1.0
+    }
+    
+    
+    var body: some View {
+        NavigationView {
+            
+            ZStack {
+                // Background gradient
+                LinearGradient(gradient: Gradient(colors: [Color("LColor4"), Color("Black")]), startPoint: .top, endPoint: .bottom)
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    // Motivational Quote
+                    VStack(spacing: 10) {
+                        Text("Quote of the Hour")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                        
+                        Spacer()
+                    }
+                }
+            }
         }
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
